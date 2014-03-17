55108964d68b40034a62907ba741ab24aa215ff7
girtonman <girtonman@gmail.com>
2014-03-17 15:30:11 -0500

    Added a custom exception. Worked on Trucks.java

---
+++ Truck Sim 9000/src/edu/auburn/scottandjoe/FatalTruckException.java
@@ -0,0 +1,12 @@
+package edu.auburn.scottandjoe;
+
+public class FatalTruckException extends Exception {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public FatalTruckException(String message) {
+        super(message);
+    }
+}
--- Truck Sim 9000/src/edu/auburn/scottandjoe/Truck.java
+++ Truck Sim 9000/src/edu/auburn/scottandjoe/Truck.java
@@ -2,48 +2,110 @@ package edu.auburn.scottandjoe;
 
 public class Truck {
 	static final double MAX_ACCELERATION = 1.0;
-	static final double MIN_ACCELERATION = -1.0;
+	static final double MIN_ACCELERATION = -3.0;
+	static final int MAX_LANE = 2;
+	static final int MIN_LANE = 1;
+	
+	public static final int RANDOMIZE_INT = -10000;
+	public static final double RANDOMIZE_DOUBLE = -10000.0;
+	
 	int tickRate = TheAir.TICK_RATE;
 	int lane;
 	int truckNumber;
 	int messagesPerSecond;
+	int desiredLane;
+	double acceleration;
 	double pos;
 	double speed;
 	boolean hasGuns = false;
-
-	public Truck(int truckNumber)
+	boolean intentChangeLane = false;
+	
+	//initializes a truck object. truck numbering conflicts are not handled, and are the truck runners responsibility
+	public Truck(int truckNumber, int lane, double pos, double speed, double acceleration)
 	{
+		this.truckNumber = truckNumber;
+		if(lane != RANDOMIZE_INT)
+		{
+			if(lane > MAX_LANE || lane < MIN_LANE)
+			{
+				System.out.println("[SEVERE] Truck " + truckNumber + " is off the road!");
+				explode("Truck was initialized in an invalid lane (allowable is " + MIN_LANE + " to " + MAX_LANE);
+			}
+			this.lane = lane;
+			this.desiredLane = lane;
+		}
+		else
+		{
+			//randomize lane accordingly
+			lane = MIN_LANE + (int)(Math.random() * ((MAX_LANE - MIN_LANE) + 1));
+		}
 		
+		if(pos != RANDOMIZE_DOUBLE)
+		{
+			this.pos = pos;
+		}
+		else
+		{
+			//TODO:randomize position accordingly
+		}
+		
+		if(speed != RANDOMIZE_DOUBLE)
+		{
+			this.speed = speed;
+		}
+		else
+		{
+			//TODO:randomize speed accordingly
+		}
+		
+		if(acceleration != RANDOMIZE_DOUBLE)
+		{
+			if(acceleration > MAX_ACCELERATION || acceleration < MIN_ACCELERATION)
+			{
+				this.acceleration = acceleration;
+			}
+			else
+			{
+				System.out.println("[SEVERE] Truck " + truckNumber + "'s engine has " +
+						"overheated and exploded from excessive acceleration/deceleration.");
+				explode("Excessive acceleration/deceleration in initialization (allowable is " + MIN_ACCELERATION + " to " + MAX_ACCELERATION + ")");
+			}
+		}
 	}
 	
-	private void accelerate(double rate)
+	private void accelerate()
 	{
-		if(rate <= MAX_ACCELERATION && rate >= MIN_ACCELERATION)
+		if(acceleration <= MAX_ACCELERATION && acceleration >= MIN_ACCELERATION)
 		{
 			//accelerate relative to tick rate
+			speed = speed + (acceleration / (double)tickRate);
 		}
 		else
 		{
 			System.out.println("[SEVERE] Truck " + truckNumber + "'s engine has " +
 					"overheated and exploded from excessive acceleration/deceleration.");
-			explode();
+			explode("Excessive acceleration/deceleration (allowable is " + MIN_ACCELERATION + " to " + MAX_ACCELERATION + ")");
 		}
 	}
 	
-	private void explode()
+	private void explode(String reason)
 	{
-		
-	}
-	private void updatePosition()
-	{
-
+		throw new FatalTruckException(reason);
 	}
 	
+	private void updatePosition()
+	{
+		//TODO:update truck position relative to speed and tick rate
+	}
+	
+	//collisions of trucks is a responsibility of the air to decide
 	public int updatePhysical()
 	{
 		//update position
+		updatePosition();
 		//update acceleration
-		//update lane
+		accelerate();
+		//TODO:update lane
 		
 		return 1;
 	}
@@ -80,7 +142,29 @@ public class Truck {
 	{
 		this.hasGuns = guns;
 	}
+	
+	public void setAcceleration(double acceleration)
+	{
+		this.acceleration = acceleration;
+	}
+	
+	public void setLaneIntent(int desiredLane)
+	{
+		intentChangeLane = true;
+		this.desiredLane = desiredLane;
+	}
+	
+	
+	
+	
+	//TODO:messages to be broadcasted. will update after reading project requirements again
+	private class Message
+	{
+		public Message()
+		
+	}
 }
+
 
 
 
