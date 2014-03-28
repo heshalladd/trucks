30314d3bea826fbb8afbab094c1248f7dbb4e011
girtonman <girtonman@gmail.com>
2014-03-28 00:21:42 -0500

    Truck.java still needs decision algorithms
    Truck runner moved forward a bit. Truck.java pretty much finished except
    for the AI.

--- Truck Sim 9000/src/edu/auburn/scottandjoe/Truck.java
+++ Truck Sim 9000/src/edu/auburn/scottandjoe/Truck.java
@@ -1,168 +1,212 @@
 package edu.auburn.scottandjoe;
 
-public class Truck {
-	static final double MAX_ACCELERATION = 1.0;
-	static final double MIN_ACCELERATION = -3.0;
-	static final int MAX_LANE = 2;
-	static final int MIN_LANE = 1;
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class Truck{
+   static final double MAX_ACCELERATION = 1.0;
+   static final double MIN_ACCELERATION = -3.0;
+   static final int MAX_LANE = 2;
+   static final int MIN_LANE = 1;
 	
-	public static final int RANDOMIZE_INT = -10000;
-	public static final double RANDOMIZE_DOUBLE = -10000.0;
+   public static final int RANDOMIZE_INT = -10000;
+   public static final double RANDOMIZE_DOUBLE = -10000.0;
 	
-	int tickRate = TheAir.TICK_RATE;
-	int lane;
-	int truckNumber;
-	int messagesPerSecond;
-	int desiredLane;
-	double acceleration;
-	double pos;
-	double speed;
-	boolean hasGuns = false;
-	boolean intentChangeLane = false;
+   int tickRate = TheAir.TICK_RATE;
+   
+   //truck meta
+   int desiredLane;
+   int sequenceNumber = 1;
+   int messagesPerSecond;
+   String convoyID = UUID.randomUUID().toString();
+   
+   //truck properties
+   double acceleration;
+   double pos;
+   double speed = 0;
+   int truckNumber;
+   int lane;
+   
+   //intents
+   boolean intentChangeLane = false;
+   
+   Message theMessage = new Message();
+   ArrayList truckCache = new ArrayList<Truck>();
 	
 	//initializes a truck object. truck numbering conflicts are not handled, and are the truck runners responsibility
-	public Truck(int truckNumber, int lane, double pos, double speed, double acceleration)
-	{
-		this.truckNumber = truckNumber;
-		if(lane != RANDOMIZE_INT)
-		{
-			if(lane > MAX_LANE || lane < MIN_LANE)
-			{
-				System.out.println("[SEVERE] Truck " + truckNumber + " is off the road!");
-				explode("Truck was initialized in an invalid lane (allowable is " + MIN_LANE + " to " + MAX_LANE);
-			}
-			this.lane = lane;
-			this.desiredLane = lane;
-		}
-		else
-		{
-			//randomize lane accordingly
-			lane = MIN_LANE + (int)(Math.random() * ((MAX_LANE - MIN_LANE) + 1));
-		}
-		
-		if(pos != RANDOMIZE_DOUBLE)
-		{
-			this.pos = pos;
-		}
-		else
-		{
-			//TODO:randomize position accordingly
-		}
-		
-		if(speed != RANDOMIZE_DOUBLE)
-		{
-			this.speed = speed;
-		}
-		else
-		{
-			//TODO:randomize speed accordingly
-		}
-		
-		if(acceleration != RANDOMIZE_DOUBLE)
-		{
-			if(acceleration > MAX_ACCELERATION || acceleration < MIN_ACCELERATION)
-			{
-				this.acceleration = acceleration;
-			}
-			else
-			{
-				System.out.println("[SEVERE] Truck " + truckNumber + "'s engine has " +
-						"overheated and exploded from excessive acceleration/deceleration.");
-				explode("Excessive acceleration/deceleration in initialization (allowable is " + MIN_ACCELERATION + " to " + MAX_ACCELERATION + ")");
-			}
-		}
-	}
+   public Truck(int truckNumber, int lane, double pos, double speed, double acceleration) throws FatalTruckException
+   {
+      Random rand = new Random();
+      this.truckNumber = truckNumber;
+      if(lane != RANDOMIZE_INT)
+      {
+         if(lane > MAX_LANE || lane < MIN_LANE)
+         {
+            System.out.println("[SEVERE] Truck " + truckNumber + " is off the road!");
+            explode("Truck was initialized in an invalid lane (allowable is " + MIN_LANE + " to " + MAX_LANE);
+         }
+         this.lane = lane;
+         this.desiredLane = lane;
+         System.out.println("[NORMAL] Truck lane initialized to " + this.lane + ".");
+      }
+      else
+      {
+      	//randomize lane accordingly
+         lane = rand.nextInt((MAX_LANE - MIN_LANE) + 1) + MIN_LANE;
+         System.out.println("[NORMAL] Truck lane randomly initialized to " + this.lane + ".");
+      }
+   	
+      if(pos != RANDOMIZE_DOUBLE)
+      {
+         this.pos = pos;
+         System.out.println("[NORMAL] Truck position initialized to " + this.pos + ".");
+      }
+      else
+      {
+         double MAX_INITIAL_POS = 350;
+      	//randomize position between 0 and 350
+         this.pos = MAX_INITIAL_POS*rand.nextDouble();
+         System.out.println("[NORMAL] Truck position randomly initialized to " + this.pos + ".");
+      }
+   	
+      if(speed != RANDOMIZE_DOUBLE)
+      {
+         this.speed = speed;
+         System.out.println("[NORMAL] Truck speed initialized to " + this.speed + ".");
+      }
+      else
+      {
+         //randomize speed accordingly
+         //55mph is 24.6m/s
+         double MIN_REASONABLE_SPEED = 24.6;
+         //80mph is 35.7m/s
+         double MAX_REASONABLE_SPEED = 35.7;
+         //these will mark the discard boundaries
+         double mean = 30;
+         double std = 10;
+         
+         int tries = 0;
+         while(this.speed < MIN_REASONABLE_SPEED || this.speed > MAX_REASONABLE_SPEED)
+         {
+            this.speed = mean + std*rand.nextGaussian();
+            tries++;
+         }
+         System.out.println("[NORMAL] Truck speed randomly initialized to " + this.speed + " after " + " tries.");
+      }
+   	
+      if(acceleration != RANDOMIZE_DOUBLE)
+      {
+         if(acceleration > MAX_ACCELERATION || acceleration < MIN_ACCELERATION)
+         {
+            this.acceleration = acceleration;
+            System.out.println("[NORMAL] Truck acceleration initialized to " + this.acceleration + ".");
+         }
+         else
+         {
+            System.out.println("[SEVERE] Truck " + truckNumber + "'s engine has " +
+               	"overheated and exploded from excessive acceleration/deceleration.");
+            explode("Excessive acceleration/deceleration in initialization (allowable is " + MIN_ACCELERATION + " to " + MAX_ACCELERATION + ")");
+         }
+      }
+      else
+      {
+         //randomize acceleration
+         this.acceleration = rand.nextDouble()*(MAX_ACCELERATION - MIN_ACCELERATION) + MIN_ACCELERATION;
+         System.out.println("[NORMAL] Truck acceleration randomly initialized to " + this.acceleration + ".");
+      }
+   }
 	
-	private void accelerate()
-	{
-		if(acceleration <= MAX_ACCELERATION && acceleration >= MIN_ACCELERATION)
-		{
-			//accelerate relative to tick rate
-			speed = speed + (acceleration / (double)tickRate);
-		}
-		else
-		{
-			System.out.println("[SEVERE] Truck " + truckNumber + "'s engine has " +
-					"overheated and exploded from excessive acceleration/deceleration.");
-			explode("Excessive acceleration/deceleration (allowable is " + MIN_ACCELERATION + " to " + MAX_ACCELERATION + ")");
-		}
-	}
+   private void accelerate() throws FatalTruckException
+   {
+      if(acceleration <= MAX_ACCELERATION && acceleration >= MIN_ACCELERATION)
+      {
+      	//accelerate relative to tick rate
+         speed = speed + (acceleration / (double)tickRate);
+      }
+      else
+      {
+         System.out.println("[SEVERE] Truck " + truckNumber + "'s engine has " +
+            	"overheated and exploded from excessive acceleration/deceleration.");
+         explode("Excessive acceleration/deceleration (allowable is " + MIN_ACCELERATION + " to " + MAX_ACCELERATION + ")");
+      }
+   }
 	
-	private void explode(String reason)
-	{
-		throw new FatalTruckException(reason);
-	}
+   private void explode(String reason) throws FatalTruckException
+   {
+      throw new FatalTruckException(reason);
+   }
 	
-	private void updatePosition()
-	{
-		//TODO:update truck position relative to speed and tick rate
-	}
+   //note: update position before acceleration
+   private void updatePosition()
+   {
+   	//update truck position relative to speed and tick rate
+      this.pos = this.pos + this.speed/tickRate;
+   }
 	
 	//collisions of trucks is a responsibility of the air to decide
-	public int updatePhysical()
-	{
-		//update position
-		updatePosition();
-		//update acceleration
-		accelerate();
-		//TODO:update lane
-		
-		return 1;
-	}
+   public int updatePhysical() throws FatalTruckException
+   {
+   	//update position
+      updatePosition();
+   	//update acceleration
+      accelerate();
+   	//TODO:update lane
+   	
+      return 1;
+   }
 	
-	public int updateDesires()
-	{
-		//update countdown to sending a broadcast to other trucks
-		//judge current state of surroundings and figure out next step towards forming a convoy
-		return 1;
-	}
+   public int updateDesires()
+   {
+   	//TODO:(maybe)update countdown to sending a broadcast to other trucks
+   	//TODO:judge current state of surroundings and figure out next step towards forming a convoy
+      return 1;
+   }
 
-	public void setLane(int lane)
-	{
-		this.lane = lane;
-	}
+   public void setLane(int lane)
+   {
+      this.lane = lane;
+   }
 
-	public void setPos(int pos)
-	{
-		this.pos = pos;
-	}
+   public void setPos(int pos)
+   {
+      this.pos = pos;
+   }
 
-	public void setSpeed(double speed)
-	{
-		this.speed = speed;
-	}
+   public void setSpeed(double speed)
+   {
+      this.speed = speed;
+   }
 
 	//set messages per second
-	public void setMPS(int mps)
-	{
-		this.messagesPerSecond = mps;
-	}
-
-	public void setGuns(boolean guns)
-	{
-		this.hasGuns = guns;
-	}
+   public void setMPS(int mps)
+   {
+      this.messagesPerSecond = mps;
+   }
 	
-	public void setAcceleration(double acceleration)
-	{
-		this.acceleration = acceleration;
-	}
+   public void setAcceleration(double acceleration)
+   {
+      this.acceleration = acceleration;
+   }
 	
-	public void setLaneIntent(int desiredLane)
-	{
-		intentChangeLane = true;
-		this.desiredLane = desiredLane;
-	}
+   public void setLaneIntent(int desiredLane)
+   {
+      intentChangeLane = true;
+      this.desiredLane = desiredLane;
+   }
 	
 	
 	
 	
 	//TODO:messages to be broadcasted. will update after reading project requirements again
-	private class Message
-	{
-		public Message()
-		
-	}
+   private class Message
+   {
+      public Message()
+      {
+         
+      }
+   	
+   }
 }
 
 
--- Truck Sim 9000/src/edu/auburn/scottandjoe/TruckRunner.java
+++ Truck Sim 9000/src/edu/auburn/scottandjoe/TruckRunner.java
@@ -1,32 +1,45 @@
 package edu.auburn.scottandjoe;
 
 public class TruckRunner {
-	static int tickRate = TheAir.TICK_RATE;
+   static int tickRate = TheAir.TICK_RATE;
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args)
-	{
-		boolean restarted = false;
-		//interpret arguments
-
-		//initialize trucks
-		while(true)
-			//initialize trucks
-			
-			//TODO:open a waiting socket and wait to be started by the server
-
-			
-			while(!restarted)
-			{
-				//TODO:send status and desired transmissions
-
-				//TODO:receive response
-
-				//TODO:do truck updates if messages are received
-				
-				//TODO:update restarted flag if the air has mandated it.
-			}
-	}
+   public static void main(String[] args)
+   {
+      boolean restarted = false;
+      int truckNumber = 0;
+      int lane = Truck.RANDOMIZE_INT;
+      double pos = Truck.RANDOMIZE_DOUBLE;
+      double speed = Truck.RANDOMIZE_DOUBLE;
+      double acceleration = Truck.RANDOMIZE_DOUBLE;
+   	//TODO:interpret arguments
+      try
+      {
+         while(true)
+         {
+         //initialize truck
+            Truck theTruck = new Truck(truckNumber, lane, pos, speed, acceleration);
+         
+         //TODO:open a waiting socket and wait to be started by the server
+         
+         
+            while(!restarted)
+            {
+            //TODO:send status and desired transmissions
+            
+            //TODO:receive response
+            
+            //TODO:do truck updates if messages are received
+            
+            //TODO:update restarted flag if the air has mandated it.
+            }
+         }
+      }
+      catch(FatalTruckException e)
+      {
+         System.out.println("[CRITICAL] " + e );
+      }
+   }
 
 }
