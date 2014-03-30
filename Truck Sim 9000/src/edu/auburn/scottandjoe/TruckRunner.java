9600eb5978690568db339d3cb9ddcec503f3efb5
girtonman <girtonman@gmail.com>
2014-03-29 22:15:38 -0500

    Finished Truck runner except for config loading

--- Truck Sim 9000/src/edu/auburn/scottandjoe/TruckRunner.java
+++ Truck Sim 9000/src/edu/auburn/scottandjoe/TruckRunner.java
@@ -1,45 +1,128 @@
 package edu.auburn.scottandjoe;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
 public class TruckRunner {
-   static int tickRate = TheAir.TICK_RATE;
+	private static int tickRate = TheAir.TICK_RATE;
+	private static boolean restarted = false;
+	private static Truck theTruck;
+
 	/**
 	 * @param args
+	 * @throws IOException
 	 */
-   public static void main(String[] args)
-   {
-      boolean restarted = false;
-      int truckNumber = 0;
-      int lane = Truck.RANDOMIZE_INT;
-      double pos = Truck.RANDOMIZE_DOUBLE;
-      double speed = Truck.RANDOMIZE_DOUBLE;
-      double acceleration = Truck.RANDOMIZE_DOUBLE;
-   	//TODO:interpret arguments
-      try
-      {
-         while(true)
-         {
-         //initialize truck
-            Truck theTruck = new Truck(truckNumber, lane, pos, speed, acceleration);
-         
-         //TODO:open a waiting socket and wait to be started by the server
-         
-         
-            while(!restarted)
-            {
-            //TODO:send status and desired transmissions
-            
-            //TODO:receive response
-            
-            //TODO:do truck updates if messages are received
-            
-            //TODO:update restarted flag if the air has mandated it.
-            }
-         }
-      }
-      catch(FatalTruckException e)
-      {
-         System.out.println("[CRITICAL] " + e );
-      }
-   }
+
+	// args[0] - "the air" ip
+	// args[1] - "the air" port
+	// args[2] - truck number (1 - 5)
+	// args[3] - config file
+	public static void main(String[] args) throws IOException {
+		String airIP = args[0];
+		int airPort = Integer.decode(args[1]);
+
+		long tickStart = 0l;
+
+		int truckNumber = Integer.decode(args[2]);
+		int lane = Truck.RANDOMIZE_INT;
+		double pos = Truck.RANDOMIZE_DOUBLE;
+		double speed = Truck.RANDOMIZE_DOUBLE;
+		double acceleration = Truck.RANDOMIZE_DOUBLE;
+		// TODO:load from config
+		try {
+			while (true) {
+				// initialize truck
+				theTruck = new Truck(truckNumber, lane, pos, speed,
+						acceleration);
+
+				// open a waiting socket and wait to be started by the server
+				InetAddress addr = InetAddress.getByName(airIP);
+				System.out.println("[NORMAL] Truck " + truckNumber
+						+ ": Air IP address: " + addr);
+				Socket tcpSock = new Socket(addr, airPort);
+				try {
+					// start a listener for the restart signal
+					new RestartListener(tcpSock).start();
+					// start thread for doing message handoffs to air, and also
+					// telling the truck to listen for broadcasts
+					new MessageHandoffHandler(tcpSock).start();
+
+					// while loop to do tick limited truck updates
+					while (!restarted) {
+						tickStart = System.nanoTime();
+						theTruck.updateDesires();
+						theTruck.updatePhysical();
+						while (((System.nanoTime() - tickStart) / 1000000) < (1 / tickRate)) {
+						}
+					}
+				} finally {
+					System.out.println("[NORMAL] Closing tcp socket...");
+					tcpSock.close();
+				}
+			}
+		} catch (FatalTruckException e) {
+			System.out.println("[CRITICAL] " + e);
+		}
+	}
+
+	private static class RestartListener extends Thread {
+		private Socket airSocket;
+
+		public RestartListener(Socket airSocket) {
+			this.airSocket = airSocket;
+		}
+
+		public void run() {
+			try {
+				BufferedReader in = new BufferedReader(new InputStreamReader(
+						airSocket.getInputStream()));
+				String restartMessage = in.readLine();
+				if (restartMessage.equals("restart")) {
+					restarted = true;
+				}
+
+			} catch (IOException e) {
+				System.out
+						.println("[SEVERE] IOException in thread that waits for reset signal.");
+			}
+		}
+
+	}
+
+	private static class MessageHandoffHandler extends Thread {
+		private Socket airSocket;
+		private String truckMessage = "";
+
+		public MessageHandoffHandler(Socket airSocket) {
+			this.airSocket = airSocket;
+		}
+
+		public void run() {
+			try {
+				while (!restarted) {
+					PrintWriter out = new PrintWriter(
+							new BufferedWriter(new OutputStreamWriter(
+									airSocket.getOutputStream())), true);
+
+					// tell truck to receive response
+					theTruck.handleMessage();
+					// send message to the air for appropriate forwarding
+					if (!truckMessage.equals("")) {
+						out.println(truckMessage);
+					}
+				}
+			} catch (IOException e) {
+				System.out
+						.println("[SEVERE] IOException in thread that handles message handoff.");
+			}
+		}
+
+	}
 
 }
