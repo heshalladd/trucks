7a47d7879ebc5ed12f7924a27ddf908b97771176
Joseph Shanahan <shanajp@auburn.edu>
2014-04-26 13:52:54 -0500

    [completes #70217494] queue received packets in database

--- VANET Visualizer/DataBase.java
+++ VANET Visualizer/DataBase.java
@@ -1,9 +1,22 @@
+import java.net.DatagramPacket;
+import java.util.concurrent.ArrayBlockingQueue;
+
 
 public class DataBase {
 	 int x1 = 0;
 	 int x2 = 0;
+	 
+	 ArrayBlockingQueue<DatagramPacket> mIncomingPackets;
+	 
 	 public DataBase(){
-		 x1 = 1;
-		 x2 = 2;
+		 mIncomingPackets = new ArrayBlockingQueue<DatagramPacket>(5096);
 	 }
+	 protected void queuePacket(DatagramPacket packet){
+		 try {
+			mIncomingPackets.put(packet);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	 }
+
 }
--- VANET Visualizer/ReceivingController.java
+++ VANET Visualizer/ReceivingController.java
@@ -11,42 +11,31 @@ public class ReceivingController {
 
 	}
 	
+	public void connectDataBase(DataBase dataBase){
+		mDataBase = dataBase;
+	}
+	
 	public void startUDPListener(){
 		try {
 			System.out.println("Starting UDP Listener...");
 			DatagramSocket serverSocket = new DatagramSocket(9876);
 			byte[] receiveData = new byte[1024];
-			byte[] sendData = new byte[1024];
-			StringTokenizer token;
-			String id;
-			while (true) {
+			//byte[] sendData = new byte[1024];
 
+			while (true) {
 				DatagramPacket receivePacket = new DatagramPacket(receiveData,
 						receiveData.length);
 				serverSocket.receive(receivePacket);
-				String sentence = new String(receivePacket.getData());
-
-				System.out.println("RECEIVED: " + sentence);
-				token = new StringTokenizer(sentence,",");
-				id = token.nextToken();
-				System.out.println(id);
-				int loc = Integer.parseInt(token.nextToken());
-				if(id.equals("1")){
-					mDataBase.x1 = loc;
-				}
-				if(id.equals("2")){
-					mDataBase.x2 = loc;
-				}
-
-				InetAddress IPAddress = receivePacket.getAddress();
-				int port = receivePacket.getPort();
-				
-				sendData = sentence.getBytes();
-				DatagramPacket sendPacket = new DatagramPacket(sendData,
-						sendData.length, IPAddress, port);
-				
-				serverSocket.send(sendPacket);
+				mDataBase.queuePacket(receivePacket);
 				receiveData = new byte[1024];
+			
+				//String sentence = new String(receivePacket.getData());
+				//InetAddress IPAddress = receivePacket.getAddress();
+				//int port = receivePacket.getPort();
+				//sendData = sentence.getBytes();
+				//DatagramPacket sendPacket = new DatagramPacket(sendData,
+				//sendData.length, IPAddress, port);
+				//serverSocket.send(sendPacket);
 			}
 
 		} catch (Exception e) {
